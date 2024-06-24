05d885736b7a30ba2fd622930b43de916d8e43fa
Shrey <Shreykoradiya200310mail.com>
2024-06-24 14:42:37 +0530

    Few Changes

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import NovicEmblem from "../assets/NoviceFigma.png";
 import NovicLogo from "../assets/NovicLogo.png";
+import GoogleLogo from "../assets/Google.svg";
+import GitHubLogo from "../assets/GitHub Logo.svg";
+import MicrosoftLogo from "../assets/Microsoft Logo.svg";
 import "../styles/Login.css";
 
 const Login = () => {
@@ -25,6 +28,26 @@ const Login = () => {
           placeholder="Password"
           required
         />
+
+        <button className="LoginButton">Login</button>
+
+        <text className="LoginW">Login With:-</text>
+
+        <div className="LoginOptions">
+          <div>
+            <img className="Options" src={GoogleLogo}></img>
+          </div>
+          <div>
+            <img className="Options" src={GitHubLogo}></img>
+          </div>
+          <div>
+            <img className="Options" src={MicrosoftLogo}></img>
+          </div>
+        </div>
+
+        <div className="NewUser">
+          New User ? <span className="RegisterURl">Register</span>{" "}
+        </div>
       </div>
     </div>
   );
