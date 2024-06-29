eefc7bbd8819739121c49fda83ac786dfdfabc75
Shrey <Shreykoradiya200310mail.com>
2024-06-29 18:02:07 +0530

    Added Utility JS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import Login from "./components/Login";
 import Signin from "./components/Signin";
+import TextEditorToolBar from "./components/TextEditorToolBar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Signin />
+      <TextEditorToolBar />
     </>
   );
 }
---
+++ frontend/src/assets/Utility.js
@@ -0,0 +1,45 @@
+import Undo from "./ToolBar_Image/Undo.svg";
+import Redo from "./ToolBar_Image/Redo.svg";
+import AlignCenter from "./ToolBar_Image/AlignCenter.svg";
+import AlignLeft from "./ToolBar_Image/AlignLeft.svg";
+import AlignRight from "./ToolBar_Image/AlignRight.svg";
+import Arrow from "./ToolBar_Image/Arrow.svg";
+import Bold from "./ToolBar_Image/Bold.svg";
+import Bullet_Numbers from "./ToolBar_Image/Bullet_Numbers.svg";
+import Bullets from "./ToolBar_Image/Bullets.svg";
+import Download from "./ToolBar_Image/cloud_download.svg";
+import Find_Replace from "./ToolBar_Image/find-replace.svg";
+import Highlight from "./ToolBar_Image/Highlight.svg";
+import JustifyText from "./ToolBar_Image/JustifyText.svg";
+import Line_Spacing from "./ToolBar_Image/Line_Spacing.svg";
+import Print from "./ToolBar_Image/Print.svg";
+import StrikeThrough from "./ToolBar_Image/Strikethrough.svg";
+import Table from "./ToolBar_Image/Table.svg";
+import TextColor from "./ToolBar_Image/TextColor.svg";
+import Underline from "./ToolBar_Image/Underline.svg";
+import Upload from "./ToolBar_Image/upload_file.svg";
+
+const ToolBarImage = {
+  Undo: Undo,
+  Redo: Redo,
+  AlignCenter: AlignCenter,
+  AlignLeft: AlignLeft,
+  AlignRight: AlignRight,
+  Arrow: Arrow,
+  Bold: Bold,
+  Bullet_Numbers: Bullet_Numbers,
+  Bullets: Bullets,
+  Download: Download,
+  Find_Replace: Find_Replace,
+  Highlight: Highlight,
+  JustifyText: JustifyText,
+  Line_Spacing: Line_Spacing,
+  Print: Print,
+  StrikeThrough: StrikeThrough,
+  Table: Table,
+  TextColor: TextColor,
+  Underline: Underline,
+  Upload: Upload,
+};
+
+export default ToolBarImage;
